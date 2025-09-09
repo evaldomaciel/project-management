a5277051d02f1624af5eb2fea54e3738873e3482
Evaldo Maciel <evaldomaciel17@gmail.com>
2025-09-09 11:31:15 -0300

    feat: new button to add ticket via board

--- app/Filament/Pages/Scrum.php
+++ app/Filament/Pages/Scrum.php
@@ -4,9 +4,14 @@ namespace App\Filament\Pages;
 
 use App\Helpers\KanbanScrumHelper;
 use App\Models\Project;
+use App\Models\Ticket;
+use App\Models\TicketPriority;
+use App\Models\TicketStatus;
+use App\Models\TicketType;
 use Filament\Facades\Filament;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Actions\Action;
 use Filament\Pages\Page;
 use Illuminate\Contracts\Support\Htmlable;
@@ -54,6 +59,40 @@ class Scrum extends Page implements HasForms
                 ->label(__('Manage sprints'))
                 ->color('primary')
                 ->url(route('filament.resources.projects.edit', $this->project)),
+
+            Action::make('create-ticket')
+                ->button()
+                ->visible(fn() => $this->project->currentSprint && auth()->user()->can('Create ticket'))
+                ->label(__('Create ticket'))
+                ->color('primary')
+                ->form([
+                    TextInput::make('name')
+                        ->label(__('Ticket name'))
+                        ->required(),
+                ])
+                ->action(function (array $data) {
+                    $defaultStatus = $this->project->status_type === 'custom'
+                        ? TicketStatus::where('project_id', $this->project->id)->where('is_default', true)->first()?->id
+                        : TicketStatus::whereNull('project_id')->where('is_default', true)->first()?->id;
+                    $defaultType = TicketType::where('is_default', true)->first()?->id;
+                    $defaultPriority = TicketPriority::where('is_default', true)->first()?->id;
+
+                    Ticket::create([
+                        'name' => $data['name'],
+                        'content' => '',
+                        'owner_id' => auth()->user()->id,
+                        'responsible_id' => null,
+                        'status_id' => $defaultStatus,
+                        'project_id' => $this->project->id,
+                        'type_id' => $defaultType,
+                        'priority_id' => $defaultPriority,
+                        'estimation' => null,
+                        'epic_id' => $this->project->currentSprint?->epic_id,
+                        'sprint_id' => $this->project->currentSprint?->id,
+                    ]);
+                    $this->getRecords();
+                    Filament::notify('success', __('Ticket created'));
+                }),
 
             Action::make('refresh')
                 ->button()

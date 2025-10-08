a078366354e7dcd0ecc6b97910b792ba822b76eb
Evaldo Maciel <evaldomaciel@to-brasil.com>
2025-10-08 13:26:43 -0300

    feat: agenda option for role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,5 +4,13 @@ namespace App\Models;
 
 class Role extends \Spatie\Permission\Models\Role
 {
+    protected $fillable = [
+        'name',
+        'guard_name',
+        'must_have_agenda'
+    ];
 
+    protected $casts = [
+        'must_have_agenda' => 'boolean',
+    ];
 }
